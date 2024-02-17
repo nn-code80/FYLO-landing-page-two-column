bc48fa1112f44a2dcd9c2b350f6ac9100f4ee019
nn-code80 <108633541+nn-code80@users.noreply.github.com>
2024-02-16 21:05:36 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+// const emailInputHead = document.querySelector('input');
+// const headerForm = document.querySelector('form');
+// const errorMsgSpan = document.querySelector('.error_msg_span');
+
+// console.log(emailInputHead);
+// console.log(headerForm)
+// console.log(errorMsgSpan);
+
+// headerForm.addEventListener('click', function(event) {
+// event.preventDefault();
+
+// if (emailInputHead.value === '') {
+// errorMsgSpan.innerHTML = 'Email cannot be empty';
+// emailInputHead.classList.add('error_msg_input');}
+// // } else if (emailInputHead.value.indexOf("@") < 1) {
+// //     errorMsgSpan.innerHTML = 'Please check your email'
+// //  } 
+// else {
+//     emailInputHead.value = ''
+//     errorMsgSpan.innerHTML = ''
+//     emailInputHead.classList.remove('error_msg_input')
+// }
+
+// })
+
